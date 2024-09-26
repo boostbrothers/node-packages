796e5b79b2e9461fed27e690ed59098cb5f439ff
고윤호 <lylisha@gmail.com>
2024-09-26 19:46:42 +0900

    test: error, success data

--- packages/please/test/test.ts
+++ packages/please/test/test.ts
@@ -1,10 +1,20 @@
+import {UnhandledResultError} from '@bbros/result';
 import {HttpClient} from '../src';
 import {paths} from './openapi-3-1';
 
 const please = new HttpClient<paths>('http://localhost:3000');
 
-const req = please.GET('/pet/{petId}', {
-  path: {
-    petId: 'id',
-  },
-});
+const req = await please
+  .GET('/pet/{petId}', {
+    path: {
+      petId: 'id',
+    },
+  })
+  .mapError(e => {
+    if (e instanceof UnhandledResultError) {
+      throw e;
+    }
+  })
+  .getOrThrow();
+
+req.data;
